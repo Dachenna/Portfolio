8b88c30ee1001c28ca6510b27f41ba06cbf3501d
David <davidinprograming@gmail.com>
2025-05-19 20:24:57 +0100

    modified:   index.html 	modified:   src/App.jsx 	modified:   src/components/LoadingScreen.jsx 	modified:   src/index.css 	modified:   vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
  return (
      <>
        { !isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+
+        </div>
     </>
  ) 
 }
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -10,8 +10,9 @@ export const LoadingScreen = (onComplete) => {
             setText(fullText.substring(0, index));
             index++;
 
-            if(index >fullText.length){
+            if(index >fullText.length) {
                 clearInterval(interval);
+
                 setTimeout(() => {
                     onComplete();
                 }, 1000);
@@ -23,7 +24,7 @@ export const LoadingScreen = (onComplete) => {
 
     return <div className="fixed inset-0 z-50 bg-black text-gray-200 flex flex-col items-center justify-center">
         <div className="mb-4 text-4xl font-mono font-bold">
-            Hello World 
+            {text}
             <span className="animate-blink ml-1"> | </span>
         </div>
         <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
